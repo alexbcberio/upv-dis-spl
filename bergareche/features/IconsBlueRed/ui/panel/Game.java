d41d1e5074ca077fc9415c878822e4e8e3e1f41f
Alexander BC <alexbcberio@gmail.com>
2022-02-26 00:03:10 +0100

    feat: add option to choose different X and O icons
    - The cross and circle images have two different options

---
+++ bergareche/features/IconsBlueRed/ui/panel/Game.java
@@ -0,0 +1,12 @@
+package ui.panel;
+
+import javax.swing.ImageIcon;
+
+public class Game {
+	private ImageIcon imgX = new ImageIcon(getClass().getResource("/imgs/blueX.jpg"));
+	private ImageIcon imgO = new ImageIcon(getClass().getResource("/imgs/redO.jpg"));
+	private ImageIcon imgXwin = new ImageIcon(
+			getClass().getResource("/imgs/blueXwin.jpg"));
+	private ImageIcon imgOwin = new ImageIcon(
+			getClass().getResource("/imgs/redOwin.jpg"));
+}
